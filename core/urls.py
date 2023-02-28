66b2eb2df58c49b8babd33951056536035b3afdc
hadi499 <hadi.ygcl@gmail.com>
2023-02-28 19:26:29 +0700

    jwt-protected-route

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+
+urlpatterns = [    
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]

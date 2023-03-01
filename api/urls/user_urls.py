b89d05f4364b1a1a9c0c596a42d2a5dc9e84e5b0
hadi499 <hadi.ygcl@gmail.com>
2023-03-01 16:43:04 +0700

    update-profile

--- api/urls/user_urls.py
+++ api/urls/user_urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
     path('profile/', views.getUserProfile, name="users-profile"),
+    path('profile/update/', views.updateUserProfile, name="user-profile-update"),
 ]
